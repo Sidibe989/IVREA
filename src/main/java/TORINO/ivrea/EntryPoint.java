1908e9f13db18f773277b9cefcc1ec6d24458941
Sidibe989 <saliousidibe69.sidibe@email.com>
2022-01-22 17:35:15 +0100

    SHERWOOD

--- src/main/java/TORINO/ivrea/EntryPoint.java
+++ src/main/java/TORINO/ivrea/EntryPoint.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 
 
 public class EntryPoint extends Application {
-
+ // COME TI chiama? SHOWNS HISTORY
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
